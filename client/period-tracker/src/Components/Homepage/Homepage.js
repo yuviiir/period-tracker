b6ea1f9761e6f489f8215a0c9abb84cdb9a1b54e
Yuvir Sharma <yuvir.sharma@vcontractor.co.za>
2022-07-05 17:24:32 +0200

    server structure

---
+++ client/period-tracker/src/Components/Homepage/Homepage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Homepage.css';
+
+const Homepage = () => {
+
+    return (
+        <React.Fragment>
+            <section className='homepage-wrapper'>
+                Period Tracker
+            </section>
+        </React.Fragment>
+    );
+}
+
+export default Homepage;

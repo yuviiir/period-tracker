51419e45f5e83dd7d91f20eb7f307218b19ee479
Yuvir Sharma <yuvir.sharma@vcontractor.co.za>
2022-07-07 11:37:31 +0200

    login done

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,29 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 //Components
 import Homepage from './Components/Homepage/Homepage';
 import LandingPage from './Components/LandingPage/LandingPage';
+import Footer from './Components/Common/Footer/Footer';
+import Nav from './Components/Common/Nav/Nav';
 
 
 function App() {
+  let routes = ["/home"]
   return (
-    <div className="App">
-        <BrowserRouter>
-          <Routes>
-            <Route exact path="/" element={<LandingPage/>}></Route>
-            <Route exact path="/home" element={<Homepage/>}></Route>
-          </Routes>
-        </BrowserRouter>
-    </div>
+    <React.Fragment>
+      {
+        routes.includes(window.location.pathname) ?
+          <Nav></Nav>
+        :
+          null
+      }
+      <div className="App">
+          <BrowserRouter>
+            <Routes>
+              <Route exact path="/" element={<LandingPage/>}></Route>
+              <Route exact path="/home" element={<Homepage/>}></Route>
+            </Routes>
+          </BrowserRouter>
+      </div>
+    </React.Fragment>
   );
 }
 

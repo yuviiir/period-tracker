7a9fa24a6869c0dddfe05739fe84ae299da9ea9f
schej002 <johannes.schepers@vcontractor.co.za>
2022-07-09 13:34:45 +0200

    Update image version.

--- dockerhub_deploy.bat
+++ dockerhub_deploy.bat
@@ -1,5 +1,5 @@
-docker build -t johannsbbd/bbd-grad-period-tracker-frontend:v1.1 client
-docker push johannsbbd/bbd-grad-period-tracker-frontend:v1.1
+docker build -t johannsbbd/bbd-grad-period-tracker-frontend:v1.2 client
+docker push johannsbbd/bbd-grad-period-tracker-frontend:v1.2
 
-docker build -t johannsbbd/bbd-grad-period-tracker-backend:v1.1 server
-docker push johannsbbd/bbd-grad-period-tracker-backend:v1.1
+docker build -t johannsbbd/bbd-grad-period-tracker-backend:v1.2 server
+docker push johannsbbd/bbd-grad-period-tracker-backend:v1.2

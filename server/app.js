6fe1dfd96f7e331fe0ff82b9f4d0f0832234fd5e
Abie Knepscheld <abrahamk@bbd.co.za>
2022-07-10 12:23:08 +0200

    fixed cycles API ENDPOINTS and app.js

--- server/app.js
+++ server/app.js
@@ -13,16 +13,19 @@ const historyRouter = require('./routes/history');
 const calendarRouter = require('./routes/calendar');
 const journalRouter = require('./routes/journal');
 const predictionRouter = require('./routes/prediction');
+const cycleRouter = require('./routes/cycle');
 
 app.use(express.json());
 
 //Authentication middleware
 app.use(verificationHandler);
 
+app.use('/cycle', cycleRouter);
 app.use('/history', historyRouter);
 app.use('/calendar', calendarRouter);
 app.use('/journal', journalRouter);
-app.use('/prediction', predictionRouter)
+app.use('/prediction', predictionRouter);
+
 
 app.all('*', (req, res) => {
     res.status(400).send('<h1> PAGE NOT FOUND! </h1>')

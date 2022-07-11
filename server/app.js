e57b9a3392d1fe0d002ccf9a95b307527f0c7bcb
MaddieLymei <97503724+MaddieLymei@users.noreply.github.com>
2022-07-11 17:23:20 +0200

    POST for Cycle

--- server/app.js
+++ server/app.js
@@ -1,36 +1,31 @@
-const { response } = require('express');
-const express = require('express');
+const { response } = require("express");
+const express = require("express");
 const app = express();
 
-require('dotenv').config();
+require("dotenv").config();
 
-const { verificationHandler } = require('./jwt');
+const { verificationHandler } = require("./jwt");
 
 const PORT = process.env.PORT || 3000;
 
 //ROUTER TO API
-const historyRouter = require('./routes/history');
-const calendarRouter = require('./routes/calendar');
-const journalRouter = require('./routes/journal');
-const predictionRouter = require('./routes/prediction');
-const cycleRouter = require('./routes/cycle');
+const historyRouter = require("./routes/history");
+const journalRouter = require("./routes/journal");
+const cycleRouter = require("./routes/cycle");
 
 app.use(express.json());
 
 //Authentication middleware
 app.use(verificationHandler);
 
-app.use('/cycle', cycleRouter);
-app.use('/history', historyRouter);
-app.use('/calendar', calendarRouter);
-app.use('/journal', journalRouter);
-app.use('/prediction', predictionRouter);
-app.use('/cycle', cycleRouter);
+app.use("/cycle", cycleRouter);
+app.use("/history", historyRouter);
+app.use("/journal", journalRouter);
 
-app.all('*', (req, res) => {
-    res.status(400).send('<h1> PAGE NOT FOUND! </h1>')
-})
+app.all("*", (req, res) => {
+  res.status(400).send("<h1> PAGE NOT FOUND! </h1>");
+});
 
-app.listen(PORT, () =>{
-    console.log(`Listening on port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
--- server/routes/cycle.js
+++ server/routes/cycle.js
@@ -1,13 +1,62 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require("mongodb");
 const url = process.env.MONGO_URL;
 
+const symptomsObject = [
+  "abdominal-cramps",
+  "bloating",
+  "body-aches",
+  "breakouts",
+  "food-cravings",
+  "headache",
+  "insomnia",
+  "mood-swings",
+  "tender-breasts",
+];
+const moodObject = [
+  "angry",
+  "annoyed",
+  "excited",
+  "happy",
+  "neutral",
+  "sad",
+  "sick",
+  "tired",
+  "worried",
+];
 
-router 
-.route("/all")
-.get(async (req, res) => {
-    let username = res.locals.username;
+router.route("/all").get(async (req, res) => {
+  let username = res.locals.username;
+
+  const conn = new MongoClient(url);
+  await conn.connect();
+
+  db = conn.db("PeriodTracker");
+
+  if (!db) {
+    res.status(500).send({ err: "Internal db error on get connection" });
+    return;
+  }
+
+  try {
+    let mongoRes = await db.collection("cycle").find({ username: username });
+
+    res.status(200).send(mongoRes);
+  } catch (e) {
+    res.status(500).send({ err: "Internal db error on query: " + e });
+  } finally {
+    db.close();
+  }
+});
+
+router
+  .route("/:date") //THIS IS TO SHOW THE MONTH VIEW OF THE CALENDAR
+  .post(async (req, res) => {
+    let date = req.params.date;
+    let cycleObject = req.body;
+    let symptoms = [];
+    let mood = "";
 
     const conn = new MongoClient(url);
     await conn.connect();
@@ -15,46 +64,51 @@ router
     db = conn.db("PeriodTracker");
 
     if (!db) {
-        res.status(500).send({ err: "Internal db error on get connection" });
-        return;
+      res.status(500).send({ err: "Internal db error on get connection" });
+      return;
+    }
+
+    if (!!cycleObject.symptoms) {
+      cycleObject.symptoms.forEach((symptom) => {
+        if (!symptomsObject.find(symptom)) {
+          return res
+            .status(400)
+            .send("The symptom being inserted into the database is not found.");
+        } else {
+          symptoms.push(symptom);
+        }
+      });
+      if (!!cycleObject.mood) {
+        if (!moodObject.find(cycleObject.mood)) {
+          return res
+            .status(400)
+            .send("The mood being inserted into the database is not found.");
+        } else {
+          mood = cycleObject.mood;
+        }
+      }
+      let cycleDetails = {
+        username: cycleObject.username,
+        date: date,
+        entry: cycleObject.entry,
+        symptoms: symptoms,
+        mood: mood,
+      };
+      db.collection("cycle").insertOne(cycleDetails, function (err, res) {
+        if (err) throw err;
+        console.log("1 document inserted");
+      });
     }
 
     try {
-        let mongoRes = await db.collection("cycle").find({username: username});
+      await db.collection("cycle").insertOne({ cycleDetails });
 
-        res.status(200).send(mongoRes);
-
+      res.status(200).send("Period Cycle Details have been recorded");
     } catch (e) {
-        res.status(500).send({err: "Internal db error on query: " + e})
-
+      res.status(500).send({ err: "Internal db error on query: " + e });
     } finally {
-        db.close();
+      db.close();
     }
-
-})
-
-router
-.route('/end') //THIS IS TO SHOW THE MONTH VIEW OF THE CALENDAR
-.get((req, res) => {
-     //DB QUERY COMES HERE
-
-    //res.send()
-})
-
-router
-.route('/average')
-.get((req,res) => {
-     //DB QUERY COMES HERE
-
-    //res.send()
-})
-
-router
-.route('length')
-.get((req,res) => {
-    //DB QUERY COMES HERE
-
-    //res.send()
-})
+  });
 
 module.exports = router;

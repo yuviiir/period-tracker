ffdcdea1ff5f96acf94ccf1b8eac525865df0e29
schej002 <johannes.schepers@vcontractor.co.za>
2022-07-13 13:25:03 +0200

    Implemented nginx proxy.

--- dockerhub_deploy.bat
+++ dockerhub_deploy.bat
@@ -3,3 +3,6 @@ docker push johannsbbd/bbd-grad-period-tracker-frontend:v1.3
 
 docker build -t johannsbbd/bbd-grad-period-tracker-backend:v1.3 server
 docker push johannsbbd/bbd-grad-period-tracker-backend:v1.3
+
+docker build -t johannsbbd/bbd-grad-period-tracker-proxy:v1.3 proxy
+docker push johannsbbd/bbd-grad-period-tracker-proxy:v1.3
